9efbd53007b64af86e45dc4890e9386799e62700
omrajsharma <omraj7413@gmail.com>
2023-05-10 20:51:46 +0530

    first commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import Form from "./component/Form"
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="shape-1"></div>
+        <div className="shape-2"></div>
+        <Form />
+      </div>
+    </div>
+  )
+}
+export default App
